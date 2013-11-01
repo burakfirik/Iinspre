086166e26e412fbc1b2b9640fd185d143156152f
burak firik <burakfirik@gmail.com>
2013-11-01 17:23:32 -0400

    UISegmented is hooked

--- Iinspre/ViewController.m
+++ Iinspre/ViewController.m
@@ -28,7 +28,7 @@
                     @"As slow as a wet week"];
     
     
-    NSString *plistCatPath=[[NSBundle mainBundle] pathForResource:@"quotes"  ofType:@"plist"];
+    NSString *plistCatPath=[[NSBundle mainBundle] pathForResource:@"InspreList"  ofType:@"plist"];
     
     self.movieQuotes=[NSMutableArray arrayWithContentsOfFile:plistCatPath];
     
@@ -46,11 +46,37 @@
 #pragma mark DataSourceDelegate
 
 -(IBAction)quoteButtonTapped:(id)sender{
-    int array_tot=[self.myQuotes count];
-    int index=(arc4random()%array_tot);
-    NSString *my_quote=self.myQuotes[index];
-    self.quoteText.text=[NSString stringWithFormat:@"Quote:\n\n%@",my_quote];
     
+    if(self.quoteOpt.selectedSegmentIndex==2){
+        
+        int array_tot=[self.myQuotes count];
+        int index=(arc4random()%array_tot);
+        NSString *my_quote=self.myQuotes[index];
+        self.quoteText.text=[NSString stringWithFormat:@"Quote:\n\n%@",my_quote];
+        
+
+        
+    }else{
+        NSString *selectedCategory=@"classic";
+        
+        if(self.quoteOpt.selectedSegmentIndex==1){
+            selectedCategory=@"modern";
+        }
+        
+        
+        NSPredicate *predicate=[NSPredicate predicateWithFormat:@"category == %@",selectedCategory];
+        NSArray *filteredArray=[self.movieQuotes filteredArrayUsingPredicate:predicate];
+        
+        int array_tot=[filteredArray count];
+        if(array_tot>0){
+            int index=(arc4random()%array_tot);
+            NSString *quote=filteredArray[index][@"quote"];
+            self.quoteText.text=[NSString stringWithFormat:@"Movie Quote:\n\n%@",quote];
+        }else{
+            self.quoteText.text=[NSString stringWithFormat:@"No quotes to display. "];
+        }
+    
+    }
     
 }
 @end
